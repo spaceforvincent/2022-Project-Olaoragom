7a684e51cbc6cec7bd53437c5090e622a1e4cdfe
SSAFY <jmr2882@naver.com>
2022-10-25 22:43:36 +0900

    [S07P31D109-133] feature - 달력 배치 및 작동 확인 완료

--- FE/ClimbingBear/src/screens/calendar/CalendarHome.js
+++ FE/ClimbingBear/src/screens/calendar/CalendarHome.js
@@ -1,11 +1,12 @@
 import React, {useEffect, useState} from 'react';
 import {SafeAreaView, Text, View, StyleSheet} from 'react-native';
+import {Calendar} from 'react-native-calendars';
 
 // React 와 구조 유사하며 return template 에는 View 로 무조건 감싸줘야 한다
 const CalendarHome = () => {
   return (
     <View>
-      <Text style={styles.temptext}>여긴 달력이 들어간 일정 스크린입니다!</Text>
+      <Calendar />
     </View>
   );
 };
