d5dfa7742b49f32338f6af6adf7b1d86d1828eed
Hyunjung Lee <pycbcat@gmail.com>
2022-10-28 17:04:50 +0900

    [] feature - Geojson 등산로 띄워서 style 구현

--- FE/ClimbingBear/src/components/climbing/ClimbingMap.js
+++ FE/ClimbingBear/src/components/climbing/ClimbingMap.js
@@ -4,13 +4,14 @@ import {
   Text,
   View,
   StyleSheet,
-  Button,
   Dimensions,
   PixelRatio,
   Image,
 } from 'react-native';
 // 지도 모듈 import
 import MapView, {Marker, PROVIDER_GOOGLE, Geojson} from 'react-native-maps';
+// 서체 import
+import {TextBold, TextMedium} from '../../components/common/TextFont';
 // useSelector 을 import 함으로서 우리가 만든 reducer state 에 접근 가능
 import {useSelector} from 'react-redux';
 // 지도 위에 띄울 버튼 import
@@ -18,8 +19,9 @@ import ClimbingButton from './ClimbingButton';
 import PlaceTypeButton from '../../components/climbing/PlaceTypeButton';
 
 // (임시) 포인트 찍기 확인용 선언
-import {palgongSpotData} from '../../assets/temp/PalgongData';
-import {palgongPathData} from '../../assets/temp/PalgongData';
+import tempgwanwakPath from '../../assets/temp/tempgwanwakPath';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+// import {palgongPathData} from '../../assets/temp/PalgongData';
 
 // (임시) 맵타입 바꾸기
 // (논의) Dimensions 창 크기 전역 관리
@@ -41,6 +43,59 @@ const ClimbingMap = ({latitude, longitude}) => {
   // mapType 지정할 state
   const [mapType, setMapType] = useState('standard');
 
+  // (임시) 어떻게 Geojson 구성할 지 선언해보고 store 에 정리
+
+  const [features, setFeatures] = useState(null);
+  const [pathIndex, setPathIndex] = useState(null);
+
+  const path = [];
+
+  function pathfeature() {
+    tempgwanwakPath.features.forEach(item => {
+      const feature = {
+        type: 'FeatureCollection',
+        features: [
+          {
+            type: 'Feature',
+            geometry: {
+              type: 'MultiLineString',
+              coordinates: item.geometry.paths,
+            },
+            properties: {
+              length: item.attributes.PMNTN_LT,
+              level: item.attributes.PMNTN_DFFL,
+              upmin: item.attributes.PMNTN_UPPL,
+              downmin: item.attributes.PMNTN_GODN,
+              color: 'rgba(79, 141, 86, 0.5)',
+            },
+          },
+        ],
+      };
+      path.push(feature);
+    });
+    setFeatures(path);
+  }
+
+  // 등산로 선택하는 함수 (정보 띄우기), 기존에 눌렀던 등산로와 같으면 정보 끄기 (색도)
+  function pickPath(payload) {
+    if (pathIndex == payload) {
+      setPathIndex(null);
+      features[payload].features[0].properties.color = 'rgba(79, 141, 86, 0.5)';
+    }
+    // 그 전에 선택됐던 등산로 선택해제 해야 함
+    else {
+      features[pathIndex].features[0].properties.color =
+        'rgba(79, 141, 86, 0.5)';
+      setPathIndex(payload);
+      features[payload].features[0].properties.color = 'rgba(154, 31, 31, 0.7)';
+    }
+  }
+
+  // (임시) 켜자마자 등산로 렌더링
+  useEffect(() => {
+    pathfeature();
+  }, []);
+
   return (
     <View style={styles.container}>
       <MapView
@@ -53,6 +108,7 @@ const ClimbingMap = ({latitude, longitude}) => {
         mapType={mapType}
         style={styles.map}
         showsUserLocation={true}
+        showsMyLocationButton={false}
         provider={PROVIDER_GOOGLE}>
         {/* (임시) 포인트, path 확인용 */}
         {/* Each child in a list should have a unique "key" prop 경고 해결 */}
@@ -64,15 +120,48 @@ const ClimbingMap = ({latitude, longitude}) => {
             />
           </Marker>
         ))} */}
-        {/* <Geojson
-          geojson={palgongPathData}
-          strokeColor="red"
-          fillColor="green"
-          strokeWidth={2}
-        /> */}
+        {features &&
+          features.map((item, index) => (
+            <Geojson
+              geojson={item}
+              strokeColor={item.features[0].properties.color}
+              strokeWidth={2}
+              tappable={true}
+              onPress={() => {
+                pickPath(index);
+              }}
+            />
+          ))}
       </MapView>
       <ClimbingButton setMapType={setMapType} setPlaceType={setPlaceType} />
       {placeType && <PlaceTypeButton />}
+      {pathIndex != null && (
+        <TextBold style={styles.levelinfo}>
+          {features[pathIndex].features[0].properties.level}
+        </TextBold>
+      )}
+      {pathIndex != null && (
+        <View style={styles.pathinfo}>
+          <View style={styles.semipathinfo}>
+            <TextBold style={styles.pathinfotext}>구간 길이</TextBold>
+            <TextMedium style={styles.pathinfotext}>
+              {features[pathIndex].features[0].properties.length} km
+            </TextMedium>
+          </View>
+          <View style={styles.semipathinfo}>
+            <TextBold style={styles.pathinfotext}>상행 시간</TextBold>
+            <TextMedium style={styles.pathinfotext}>
+              {features[pathIndex].features[0].properties.upmin} 분
+            </TextMedium>
+          </View>
+          <View style={styles.semipathinfo}>
+            <TextBold style={styles.pathinfotext}>하행 시간</TextBold>
+            <TextMedium style={styles.pathinfotext}>
+              {features[pathIndex].features[0].properties.downmin} 분
+            </TextMedium>
+          </View>
+        </View>
+      )}
     </View>
   );
 };
@@ -83,9 +172,9 @@ const styles = StyleSheet.create({
   // (임시) MapView 띄우는 확인 위한 공식 문서 style 에 dimensions 추가해서 화면 꽉 채우도록 설정
   container: {
     // ...StyleSheet.absoluteFillObject,
-    height: windowHeight * 0.6,
+    height: windowHeight * 0.65,
     width: windowWidth,
-    position: 'relative',
+    // position: 'relative',
   },
   map: {
     ...StyleSheet.absoluteFillObject,
@@ -95,5 +184,27 @@ const styles = StyleSheet.create({
   tempmarker: {
     height: 15,
     width: 30,
+  },
+  levelinfo: {
+    position: 'absolute',
+    fontSize: widthPixel * 0.025,
+    right: widthPixel * 0.025,
+    top: widthPixel * 0.31,
+    color: '#000000',
+  },
+  pathinfo: {
+    flexDirection: 'row',
+    paddingHorizontal: widthPixel * 0.05,
+    marginRight: widthPixel * 0.03,
+    justifyContent: 'space-evenly',
+    top: widthPixel * 0.415,
+  },
+  pathinfotext: {
+    fontSize: widthPixel * 0.017,
+    color: '#000000',
+  },
+  semipathinfo: {
+    flexDirection: 'column',
+    alignItems: 'center',
   },
 });
