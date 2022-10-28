8cc63cebb396bd4677cf170cd9102ecee69d2bee
@ue0828 <ue0828@naver.com>
2022-10-28 14:04:23 +0900

    chore - 배포 테스트

--- BE/climbingBear/src/main/java/com/example/climbingBear/ClimbingBearApplication.java
+++ BE/climbingBear/src/main/java/com/example/climbingBear/ClimbingBearApplication.java
@@ -12,6 +12,8 @@ public class ClimbingBearApplication {
 
 
 	public static void main(String[] args) {
-		SpringApplication.run(ClimbingBearApplication.class, args);}
+		SpringApplication.run(ClimbingBearApplication.class, args);
+
+	}
 
 }
