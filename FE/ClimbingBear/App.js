5b34dd938be82b901f8d43a1283ed3f9948f8c84
dearsyjang <dearsyjang@naver.com>
2022-10-31 16:18:34 +0900

    chore - 머지 충돌 방지

--- FE/ClimbingBear/App.js
+++ FE/ClimbingBear/App.js
@@ -10,7 +10,6 @@ import store from './src/store';
 import {Provider} from 'react-redux';
 // (임시) Auth 기능 보류하고 바로 지도/검색 (MapHome) 으로 가는 네비게이터 import
 import HomeNavigation from './src/navigation/HomeNavigation';
-import AuthNavigation from './src/navigation/AuthNavigation';
 
 function App() {
   // splash screen 종료시키는 코드 추가
@@ -23,11 +22,11 @@ function App() {
     <Provider store={store}>
       <NavigationContainer>
         <StatusBar backgroundColor="black" barStyle="light-content" />
-        {/* <HomeNavigation /> */}
-        <AuthNavigation />
+        <HomeNavigation />
       </NavigationContainer>
     </Provider>
   );
 }
 
 export default App;
+
