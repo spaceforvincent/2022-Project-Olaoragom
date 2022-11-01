4a85b5591153c0118abb70a6d4bd91b033964080
정윤해 <ue0828@naver.com>
2022-11-01 12:35:00 +0900

    Feature/back/user

---
+++ BE/climbingBear/src/main/java/com/example/climbingBear/domain/diary/entity/Diary.java
@@ -0,0 +1,24 @@
+package com.example.climbingBear.domain.diary.entity;
+
+import com.example.climbingBear.domain.user.entity.User;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+//@NoArgsConstructor
+//@Entity
+public class Diary {
+//    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "diary_seq")
+//    private Long diarySeq;
+//
+//    @ManyToOne
+//    private User user;
+//
+//    private String year;
+//
+//    private String month;
+//
+//    private String day;
+
+}
