45f5b9a3df31e80062a979a9acfc79f1332584fe
배근혜 <odille2992@naver.com>
2022-11-07 13:41:09 +0900

    feature - 소켓통신오류 및 해결 중

--- FE/ClimbingBear/src/screens/chat/ChatRoom.js
+++ FE/ClimbingBear/src/screens/chat/ChatRoom.js
@@ -25,6 +25,8 @@ const Chat = () => {
   const navigation = useNavigation();
   const route = useRoute();
   const [messages, setMessages] = useState([]);
+  const [serverState, setServerState] = useState('Loading...');
+
   // const [senderNick, setSenderNick] = useState(route.params.record.sender_nick)
   // const [receiverNick, setReceiverNick] = useState(route.params.record.receiver_nick)
   // const [image_path, setImage_path] = useState(route.params.record.image_path)
@@ -49,13 +51,19 @@ const Chat = () => {
   useEffect(() => {
     console.log("소켓통신 가즈아")
     // enter your websocket url
-    ws.current = new WebSocket(`wss://`)
-  //  ws.current = new WebSocket(`wss://w567l.sse.codesandbox.io/`)
+    // ws.current = new WebSocket(`wss://`)
+   ws.current = new WebSocket(`wss://w567l.sse.codesandbox.io/`)
     ws.current.onopen = () => {
       console.log("열렸다!!!")
+      setServerState('서버에 연결되었습니다.')
     };
     ws.current.onclose = () => {
       console.log("닫힘!")
+      setServerState('연결X. 인터넷이나 서버를 확인해주세요.')
+    };
+    ws.onerror = (e) => {
+      console.log(e.message)
+      setServerState(e.message);
     };
     return () => {
       ws.current.close();
@@ -65,12 +73,12 @@ const Chat = () => {
   useEffect(() => {
     setMessages([
       {
-        _nick: receiverNick, // receiver nickname
+        _id: receiverNick, // receiver nickname
         // _nick: userList[0].nickname,
-        text: '하이 에이치아이',
+        text: '등산시작!',
         createdAt: new Date(), // 현재시각
         user: {
-          _nick: senderNick,  // sender nick
+          _id: senderNick,  // sender nick
           // _nick: userList[1].nickname,         
           // avatar: image_path,
         },
@@ -79,19 +87,21 @@ const Chat = () => {
   }, [])
  
   useEffect(() => {
+    // 수신된 메시지를 병합하기
     ws.current.onmessage = e => {
       const response = JSON.parse(e.data);
       console.log("onmessage=>", JSON.stringify(response));
       let sentMessages = {
-        _nick: response.receiverNick,
+        _id: response.receiverNick,
         text: response.message,
-        createdAt: new Date(response.createdAt),
-        // createdAt: new Date(response.createdAt * 1000),
+        // createdAt: new Date(response.createdAt),
+        createdAt: new Date(response.createdAt * 1000),
         user: {          
-          _nick: response.senderNick,
+          _id: response.senderNick,
           // avatar: image_path,
         },
       }
+      // 예전 메시지와 최신 메시지 병합
       setMessages(previousMessages => GiftedChat.append(previousMessages, sentMessages))
     };
   }, []);
@@ -104,27 +114,31 @@ const Chat = () => {
       "action": "message"
     }
     ws.current.send(JSON.stringify(obj))
+    // 예전 메시지와 최신 메시지 병합
     setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
   }, [])
 
   return (
     <View style={styles.container}>
+      {/* 상단 바1 */}
       <View style={{
         padding: 15,
         marginTop: 50,
-        backgroundColor: "black",
+        backgroundColor: "#D7FBE8",
+        // color: "#858383",
         alignItems: "center",
         justifyContent: 'center',
         width: '100%'
       }}>
+        {/* back버튼 */}
         <TouchableOpacity
           style={{
             position: 'absolute',
             left: 10,
-            borderColor: "#fff",
-            borderWidth: 1,
+            // borderColor: "#858383",
+            // borderWidth: 1,
             padding: 7,
-            borderRadius: 10
+            // borderRadius: 10
           }}
           onPress={() => {
             navigation.goBack()
@@ -134,26 +148,49 @@ const Chat = () => {
             style={{
               fontSize: 15,
               fontWeight: 'bold',
-              color: "#fff",
+              color: "#858383",
             }}
           >{`Back`}</Text>
         </TouchableOpacity>
+        {/* 제목 */}
         <Text style={{
           fontSize: 20,
           fontWeight: 'bold',
-          color: "#fff"
-        }}>{`Chat list`}</Text>
+          color: "#858383"
+        }}>{`일행과 대화 중`}</Text>
       </View>
+
+      {/* 상단 바2 */}
+      <View style={{
+        padding: 6,
+        // marginTop: 18,
+        backgroundColor: "#E0E0E0",
+        // color: "#858383",
+        alignItems: "center",
+        justifyContent: 'center',
+        width: '100%'
+      }}>
+        <Text style={{
+            fontSize: 8,
+            fontWeight: 'bold',
+            color: "#fff"
+          }}>{serverState}</Text>
+      </View>
+    
+
+
       <GiftedChat
         messages={messages}
         onSend={messages => onSend(messages)}
         user={{
-          _id: senderId,  // set sender id
+          _id: senderNick,  // set sender nick
         }}
       />
     </View>
   );
 };
+
+export default Chat;
 
 const styles = StyleSheet.create({
   container: {
@@ -161,5 +198,4 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff"
   }
 });
-export default Chat;
  
