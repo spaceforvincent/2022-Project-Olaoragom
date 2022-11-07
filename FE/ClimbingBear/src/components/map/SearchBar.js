b536c0f6aab1c59d6553dceb32a2542a5ae4735e
장수영 <dearsyjang@naver.com>
2022-11-07 14:51:55 +0900

    feature - 산 지도 검색바 및 세미정보 모달창

--- FE/ClimbingBear/src/components/map/SearchBar.js
+++ FE/ClimbingBear/src/components/map/SearchBar.js
@@ -1,33 +1,93 @@
-import React, {useEffect, useState} from 'react';
-import {SafeAreaView, TextInput, View, Button, StyleSheet} from 'react-native';
+import { Fragment, useEffect, useLayoutEffect, useState } from "react";
+import { View, StyleSheet } from "react-native"
 
-// React 와 구조 유사하며 return template 에는 View 로 무조건 감싸줘야 한다
+import SearchableDropdown from 'react-native-searchable-dropdown'
 
-const SearchBar = ({setLatitude, setLongitude}) => {
-  /* 
-  (임시) 아무거나 입력했을 때 검색 버튼이 뜨고, 버튼을 누르면 위/경도 재설정 해 위치가 바뀌도록 구현
-  여기에는 검색바 구현할 예정
-  */
-  function relocate() {
-    setLatitude(37.416481);
-    setLongitude(126.884791);
+import { getMountainList } from "../../apis/Map";
+
+const SearchBar = ({navigation}) => {
+  const [ items, setItems ] = useState([])
+  const [ mountainId, setMountainId ] = useState('')
+  const [ mountainName, setMountainName ] = useState('')
+
+  // 세미 정보 모달
+  const [ modalVisible, setModalVisible ] = useState(false)
+  const modalOpen = () => {
+    setModalVisible(true)
   }
 
-  // (공부) TextInput 속성
+  // 산 리스트
+  useLayoutEffect(() => {
+    const initialData = async () => {
+      const response = await getMountainList();
+
+      let templist = []
+      response.map((mn, index) => {
+        templist.push({
+          id: mn.mntnSeq,
+          name: mn.mntnNm
+        })
+      })
+      setItems(templist)
+    }
+    initialData()
+  }, [])
+ 
   return (
     <View>
-      {/* <TextInput
-        placeholder="산을 검색하세요"
-        style={styles.temptext}
-        onSubmitEditing={() => relocate()}></TextInput> */}
+      <Fragment>
+        <SearchableDropdown
+          onItemSelect={item => {
+            setMountainId(item.id)
+            setMountainName(item.name)
+            onPress={modalOpen}
+          }}
+
+          containerStyle={{
+            backgroundColor: 'white',
+            padding: 15
+          }}
+
+          itemStyle={{
+            padding: 10,
+            marginTop: 2,
+            height: 60,
+            backgroundColor: '#F9F7F7',
+            borderColor: '#bbb',
+            borderWidth: 1,
+            borderRadius: 5,
+          }}
+          itemTextStyle={{
+            fontFamily: 'SeoulNamsanL',
+            fontSize: 20,
+            color: '#222',
+            marginTop: 10,
+          }}
+     
+          itemsContainerStyle={{maxHeight: 500}}
+          items={items}
+          defaultIndex={0}
+          resetValue={false}
+
+          textInputProps={{
+            placeholder: '산 이름을 입력해주세요.',
+            underlineColorAndroid: 'transparent',
+            style: {
+              padding: 12,
+              borderWidth: 1,
+              borderColor: '#ccc',
+              borderRadius: 5,
+              fontFamily: 'SeoulNamsanL',
+            },
+            // onTextChange: text => alert(text),
+          }}
+          listProps={{
+            nestedScrollEnabled: true,
+          }}
+        />
+      </Fragment>
     </View>
   );
-};
+}
 
 export default SearchBar;
-
-const styles = StyleSheet.create({
-  temptext: {
-    fontSize: 20,
-  },
-});
