4ea7641d26b0dcf0d4be56023c8faef6f7e4f6b8
정윤해 <ue0828@naver.com>
2022-11-07 16:42:01 +0900

    Feature/back/diary

--- BE/climbingBear/src/main/java/com/example/climbingBear/domain/mntn/dto/MntnDetailResDto.java
+++ BE/climbingBear/src/main/java/com/example/climbingBear/domain/mntn/dto/MntnDetailResDto.java
@@ -39,6 +39,9 @@ public class MntnDetailResDto {
 
     private String mntnPathImg;
 
+    private Float mntnLat;
+    private Float mntnLon;
+
     public static MntnDetailResDto ofMntnDetail(Mountain mntn, List place, String level){
         return MntnDetailResDto.builder()
                 .mntnNm(mntn.getMntnNm())
@@ -51,6 +54,8 @@ public class MntnDetailResDto {
                 .mntnTransport(mntn.getMntnTransport())
                 .mntnImg(mntn.getMntnImg())
                 .mntnPathImg(mntn.getMntnPathImg())
+                .mntnLon(mntn.getMntnLon())
+                .mntnLat(mntn.getMntnLat())
                 .place(place)
                 .level(level)
                 .build();
