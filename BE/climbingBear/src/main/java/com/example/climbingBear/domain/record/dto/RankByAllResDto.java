474e223cae033bae1224f037c2cb07a1b96b4595
정윤해 <ue0828@naver.com>
2022-11-08 17:33:10 +0900

    Feature/back/diary

--- BE/climbingBear/src/main/java/com/example/climbingBear/domain/record/dto/RankByAllResDto.java
+++ BE/climbingBear/src/main/java/com/example/climbingBear/domain/record/dto/RankByAllResDto.java
@@ -1,7 +1,22 @@
 package com.example.climbingBear.domain.record.dto;
 
+import com.example.climbingBear.domain.record.entity.Record;
+import com.example.climbingBear.domain.user.repository.UserRepository;
 import lombok.Data;
+
+import javax.naming.ldap.PagedResultsControl;
+import java.util.List;
+import java.util.Map;
 
 @Data
 public class RankByAllResDto {
+    private Long userSeq;
+    private String nickname;
+    private double distance;
+
+    public RankByAllResDto(Map.Entry<Long, Double> longDoubleEntry) {
+        this.userSeq = longDoubleEntry.getKey();
+        this.distance = longDoubleEntry.getValue();
+        this.nickname = getNickname();
+    }
 }
