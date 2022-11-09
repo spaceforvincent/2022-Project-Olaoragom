455660689315c841fc829181181553cdab725153
정윤해 <ue0828@naver.com>
2022-11-09 10:49:03 +0900

    Back

---
+++ BE/climbingBear/src/main/java/com/example/climbingBear/domain/chat/service/ChatService.java
@@ -0,0 +1,52 @@
+package com.example.climbingBear.domain.chat.service;
+
+import com.example.climbingBear.domain.chat.dto.ChatRoomListResDto;
+import com.example.climbingBear.domain.chat.dto.ChatRoomPostReqDto;
+import com.example.climbingBear.domain.chat.dto.ChatRoomPostResDto;
+import com.example.climbingBear.domain.chat.entity.ChatRoom;
+import com.example.climbingBear.domain.chat.exception.NoExistChatRoomException;
+import com.example.climbingBear.domain.chat.repository.ChatRepository;
+import com.example.climbingBear.domain.diary.dto.DiaryListResDto;
+import com.example.climbingBear.domain.user.entity.User;
+import com.example.climbingBear.domain.user.exception.NoExistUserException;
+import com.example.climbingBear.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class ChatService {
+    private final ChatRepository chatRepository;
+    private final UserRepository userRepository;
+
+    public ChatRoomPostResDto chatRoomSave (ChatRoomPostReqDto dto, Long userSeq) throws Exception {
+        User user = userRepository.findByUserSeq(userSeq).orElseThrow(() ->
+                new NoExistUserException());
+        ChatRoom chatRoom = dto.toChatEntity(user);
+        chatRepository.save(chatRoom);
+        return ChatRoomPostResDto.of(chatRoom.getChatRoomSeq());
+    }
+
+    public List<ChatRoomListResDto> chatRoomList () throws Exception {
+        List<ChatRoom> chatRooms = chatRepository.findAll();
+        return chatRooms.stream().map(ChatRoomListResDto::new).collect(Collectors.toList());
+    }
+
+    public String chatRoomDelete (Long chatRoomSeq, Long userSeq) throws Exception {
+        User user = userRepository.findByUserSeq(userSeq).orElseThrow(() ->
+                new NoExistUserException());
+        ChatRoom chatRoom = chatRepository.findByChatRoomSeq(chatRoomSeq).orElseThrow(() ->
+                new NoExistChatRoomException());
+        if (chatRoom.getUser().getUserSeq() == user.getUserSeq()){
+            chatRepository.delete(chatRoom);
+            return "삭제 완료됐습니다.";
+        } else {
+            throw new NoExistChatRoomException();
+        }
+    }
+}
