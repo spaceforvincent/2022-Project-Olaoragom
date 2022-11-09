498fc7c807be3d9d97c4cf15fc3321d4a07360ac
dearsyjang <dearsyjang@naver.com>
2022-11-09 17:05:43 +0900

    feature - 사이드바 상세설정

--- FE/ClimbingBear/src/navigation/SideBar.js
+++ FE/ClimbingBear/src/navigation/SideBar.js
@@ -8,20 +8,36 @@ import LoginScreen from '../screens/auth/LoginScreen';
 import CalendarHome from '../screens/calendar/CalendarHome';
 import ChatRoom from '../screens/chat/ChatRoom';
 import MapHome from '../screens/map/MapHome';
-import {AuthNavigation} from './AuthNavigation';
+import LogoutScreen from '../screens/auth/LogoutScreen'
+import { AuthNavigation} from './AuthNavigation';
 import { CalendarNavigation } from './CalendarNavigation';
 import { MapNavigation } from './MapNavigation';
+
+import { useSelector } from 'react-redux';
 
 const drawer = createDrawerNavigator();
 
 function DrawerNavigator() {
+  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+    
   return (
-    <drawer.Navigator useLegacyImplementation initialRouteName="Login">
-      <drawer.Screen name="Login" component={AuthNavigation} />
-      <drawer.Screen name="Calendar" component={CalendarNavigation} />
-      <drawer.Screen name="Map" component={MapNavigation} />
-      <drawer.Screen name="ChatRoom" component={ChatRoom} />
-    </drawer.Navigator>
+    <>
+      {/* 로그인O */}
+      {isAuthenticated && (
+        <drawer.Navigator useLegacyImplementation initialRouteName="Login">
+          <drawer.Screen name="Calendar" component={CalendarNavigation} />
+          <drawer.Screen name="Map" component={MapNavigation} />
+          <drawer.Screen name="ChatRoom" component={ChatRoom} />
+          <drawer.Screen name="logout" component={LogoutScreen}/>
+        </drawer.Navigator>
+      )}
+      {/* 로그인X */}
+      {isAuthenticated === false && (
+        <drawer.Navigator useLegacyImplementation initialRouteName="Login">
+          <drawer.Screen name="Login" component={AuthNavigation} options={{headerShown: false}}/>
+        </drawer.Navigator>
+      )}
+    </>
   );
 }
 
