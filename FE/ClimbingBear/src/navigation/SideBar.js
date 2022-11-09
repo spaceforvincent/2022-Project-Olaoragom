a47329cc48e5f04bbd3ca33584045f7c4afd780b
장수영 <dearsyjang@naver.com>
2022-11-09 17:22:32 +0900

    feature - 상세정보 산 이미지 작업중 / 사이드바 수정

--- FE/ClimbingBear/src/navigation/SideBar.js
+++ FE/ClimbingBear/src/navigation/SideBar.js
@@ -8,22 +8,38 @@ import LoginScreen from '../screens/auth/LoginScreen';
 import CalendarHome from '../screens/calendar/CalendarHome';
 import ChatRoom from '../screens/chat/ChatRoom';
 import MapHome from '../screens/map/MapHome';
-import {AuthNavigation} from './AuthNavigation';
+import LogoutScreen from '../screens/auth/LogoutScreen'
+import { AuthNavigation} from './AuthNavigation';
 import { CalendarNavigation } from './CalendarNavigation';
 import { MapNavigation } from './MapNavigation';
 import { ClimbingNavigation } from './ClimbingNavigation';
 
+import { useSelector } from 'react-redux';
+
 const drawer = createDrawerNavigator();
 
 function DrawerNavigator() {
+  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+    
   return (
-    <drawer.Navigator useLegacyImplementation initialRouteName="Login">
-      <drawer.Screen name="Login" component={AuthNavigation} />
-      <drawer.Screen name="Climbing" component={ClimbingNavigation} />
-      <drawer.Screen name="Calendar" component={CalendarNavigation} />
-      <drawer.Screen name="Map" component={MapNavigation} />
-      <drawer.Screen name="ChatRoom" component={ChatRoom} />
-    </drawer.Navigator>
+    <>
+      {/* 로그인O */}
+      {isAuthenticated && (
+        <drawer.Navigator useLegacyImplementation initialRouteName="Login">
+          <drawer.Screen name="Calendar" component={CalendarNavigation} />
+          <drawer.Screen name="Climbing" component={ClimbingNavigation} />
+          <drawer.Screen name="Map" component={MapNavigation} />
+          <drawer.Screen name="ChatRoom" component={ChatRoom} />
+          <drawer.Screen name="logout" component={LogoutScreen}/>
+        </drawer.Navigator>
+      )}
+      {/* 로그인X */}
+      {isAuthenticated === false && (
+        <drawer.Navigator useLegacyImplementation initialRouteName="Login">
+          <drawer.Screen name="Login" component={AuthNavigation} options={{headerShown: false}}/>
+        </drawer.Navigator>
+      )}
+    </>
   );
 }
 
