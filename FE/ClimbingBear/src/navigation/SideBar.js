624708f3b18dc774fb19c081fd403a9e90985bee
장수영 <dearsyjang@naver.com>
2022-11-10 09:21:15 +0900

    feature & fix - 산 세미정보 및 사이즈바 코드 정리

--- FE/ClimbingBear/src/navigation/SideBar.js
+++ FE/ClimbingBear/src/navigation/SideBar.js
@@ -4,15 +4,12 @@ import {
   DrawerItemList,
   DrawerItem,
 } from '@react-navigation/drawer';
-import LoginScreen from '../screens/auth/LoginScreen';
-import CalendarHome from '../screens/calendar/CalendarHome';
-import ChatRoom from '../screens/chat/ChatRoom';
-import MapHome from '../screens/map/MapHome';
-import LogoutScreen from '../screens/auth/LogoutScreen'
+
 import { AuthNavigation} from './AuthNavigation';
 import { CalendarNavigation } from './CalendarNavigation';
 import { MapNavigation } from './MapNavigation';
 import { ClimbingNavigation } from './ClimbingNavigation';
+import { ChatNavigation } from './ChatNavigation'
 
 import { useSelector } from 'react-redux';
 
@@ -20,23 +17,23 @@ const drawer = createDrawerNavigator();
 
 function DrawerNavigator() {
   const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
-    
+
   return (
     <>
-      {/* 로그인O */}
-      {isAuthenticated && (
+      {isAuthenticated ? (
         <drawer.Navigator useLegacyImplementation initialRouteName="Login">
-          <drawer.Screen name="Calendar" component={CalendarNavigation} />
-          <drawer.Screen name="Climbing" component={ClimbingNavigation} />
-          <drawer.Screen name="Map" component={MapNavigation} />
-          <drawer.Screen name="ChatRoom" component={ChatRoom} />
-          <drawer.Screen name="logout" component={LogoutScreen}/>
+          <drawer.Screen name="Calendar" options={{unmountOnBlur: true, headerTitle:''}} component={CalendarNavigation} />
+          <drawer.Screen name="Climbing" options={{unmountOnBlur: true, headerTitle:''}} component={ClimbingNavigation} />
+          <drawer.Screen name="Map" options={{unmountOnBlur: true, headerTitle:''}} component={MapNavigation} />
+          <drawer.Screen name="ChatRoom" options={{unmountOnBlur: true, headerTitle:''}} component={ChatNavigation} />
         </drawer.Navigator>
-      )}
-      {/* 로그인X */}
-      {isAuthenticated === false && (
+      ) : (
         <drawer.Navigator useLegacyImplementation initialRouteName="Login">
-          <drawer.Screen name="Login" component={AuthNavigation} options={{headerShown: false}}/>
+          <drawer.Screen
+            name="Login"
+            component={AuthNavigation}
+            options={{headerShown: false}}
+          />
         </drawer.Navigator>
       )}
     </>
