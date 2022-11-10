3dcc854fbdc5fb92eeed135faa53ab35dfe459d5
정윤해 <ue0828@naver.com>
2022-11-10 14:08:56 +0900

    Feature/back/chat

--- BE/climbingBear/src/main/java/com/example/climbingBear/domain/chat/dto/ChatRoomListResDto.java
+++ BE/climbingBear/src/main/java/com/example/climbingBear/domain/chat/dto/ChatRoomListResDto.java
@@ -1,22 +1,22 @@
-package com.example.climbingBear.domain.chat.dto;
-
-import com.example.climbingBear.domain.chat.entity.ChatRoom;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-
-@Data
-@AllArgsConstructor
-@Getter
-public class ChatRoomListResDto {
-    private Long chatRoomSeq;
-    private String title;
-
-    private Long roomOwner;
-
-    public ChatRoomListResDto (ChatRoom chatRoom){
-        this.chatRoomSeq = chatRoom.getChatRoomSeq();
-        this.title = chatRoom.getTitle();
-        this.roomOwner = chatRoom.getUser().getUserSeq();
-    }
-}
+//package com.example.climbingBear.domain.chat.dto;
+//
+//import com.example.climbingBear.domain.chat.entity.ChatRoom;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.Getter;
+//
+//@Data
+//@AllArgsConstructor
+//@Getter
+//public class ChatRoomListResDto {
+//    private Long chatRoomSeq;
+//    private String title;
+//
+//    private Long roomOwner;
+//
+//    public ChatRoomListResDto (ChatRoom chatRoom){
+//        this.chatRoomSeq = chatRoom.getChatRoomSeq();
+//        this.title = chatRoom.getTitle();
+//        this.roomOwner = chatRoom.getUser().getUserSeq();
+//    }
+//}
