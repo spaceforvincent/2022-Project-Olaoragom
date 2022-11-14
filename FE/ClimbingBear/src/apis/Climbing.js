809e95fe4dd7b7a8de1242b5ad3aa7a4995db333
hyunjung20222 <hyunjung20222@gmail.com>
2022-11-14 14:38:03 +0900

    feature - 기록 저장 axios 성공

---
+++ FE/ClimbingBear/src/apis/Climbing.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+import EncryptedStorage from 'react-native-encrypted-storage';
+
+const API_URL = 'http://k7d109.p.ssafy.io:8080/mntn';
+
+export const postClimbingData = async (
+  mntnSeq,
+  year,
+  month,
+  day,
+  time,
+  distance,
+) => {
+  const accessToken = await EncryptedStorage.getItem('accessToken');
+  try {
+    const response = await axios({
+      method: 'post',
+      url: API_URL + `/record`,
+      headers: {
+        Authorization: accessToken,
+      },
+      data: {
+        day: day,
+        distance: distance,
+        mntnSeq: mntnSeq,
+        month: month,
+        time: time,
+        year: year,
+      },
+    });
+    if (response.status === 200) {
+      console.log('데이터 저장 성공');
+      return true;
+    }
+  } catch (error) {
+    console.log(error);
+    console.log(error.response.data);
+    return false;
+  }
+};
