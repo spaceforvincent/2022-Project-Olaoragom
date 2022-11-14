abf10d4d7944db13d1c34764b0a5b90fc2136872
정윤해 <ue0828@naver.com>
2022-11-14 17:25:28 +0900

    Back

---
+++ BE/climbingBear/src/main/java/com/example/climbingBear/domain/chat/dto/ChatRoomListResDto.java
@@ -0,0 +1,27 @@
+package com.example.climbingBear.domain.chat.dto;
+
+import com.example.climbingBear.domain.chat.entity.ChatRoom;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import org.omg.CORBA.PUBLIC_MEMBER;
+
+@Data
+@Builder
+@Getter
+@AllArgsConstructor
+public class ChatRoomListResDto {
+
+    private Long roomSeq;
+    private String roomName;
+    private Long hostUser;
+    private String hostNickname;
+
+    public ChatRoomListResDto (ChatRoom chatRoom){
+        this.roomSeq = chatRoom.getChatRoomSeq();
+        this.roomName = chatRoom.getRoomName();
+        this.hostUser = chatRoom.getUser().getUserSeq();
+        this.hostNickname = chatRoom.getUser().getNickname();
+    }
+}
