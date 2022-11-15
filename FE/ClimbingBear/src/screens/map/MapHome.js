0e47cbb9e9054dbd22bf43563ed3661da76f45ef
dearsyjang <dearsyjang@naver.com>
2022-11-15 20:48:13 +0900

    feature - 산 클러스터 & 마커 최종

--- FE/ClimbingBear/src/screens/map/MapHome.js
+++ FE/ClimbingBear/src/screens/map/MapHome.js
@@ -1,42 +1,72 @@
 import React, {useEffect, useState} from 'react';
-import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
+import MapView from 'react-native-map-clustering';
+import {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
+import { Button } from '@rneui/themed';
 import Geolocation from 'react-native-geolocation-service';
-import { PermissionsAndroid, SafeAreaView, ScrollView, View, StyleSheet, Text, Dimensions } from 'react-native';
+import {TouchableOpacity, PermissionsAndroid, SafeAreaView, ScrollView, View, StyleSheet, Text, Dimensions } from 'react-native';
 import { useSelector } from 'react-redux';
 import SearchBar from '../../components/map/SearchBar';
+import { ClusterMap } from 'react-native-cluster-map';
 import { Image } from 'react-native';
+import { getMountainList } from '../../apis/Map';
+import { TextLight, TextMedium, TextBold, TextExtraBold } from '../../components/common/TextFont';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
 const MapHome = () => {
-  // GPS =>
-  const [latitude, setLatitude] = useState(36.109328);
-  const [longitude, setLongitude] = useState(128.415011);
-
+  const [cluster, setCluster] = useState(true);
+  const [markers, setMarker] = useState([]);
   const markerLat = useSelector(state => state.map.markerLat);
   const markerLon = useSelector(state => state.map.markerLon);
 
   useEffect(() => {
-    // 안드로이드 위치 설정 권한
-    const gpsPermissionAndroid = async () => {
-      if (Platform.OS === 'android') {
-        const res = await PermissionsAndroid.request(
-          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-        );
-        if (res === 'granted') {
-          Geolocation.getCurrentPosition(pos => {
-            setLatitude(pos.coords.latitude);
-            setLongitude(pos.coords.longitude);
-          });
-        }
-      }
-    };
-    gpsPermissionAndroid();
-  }, []);
+    const initialData = async() => {
+      const response = await getMountainList()
+      setMarker(response)
+      console.log(cluster)
+    }
+    initialData();
+  }, [])
 
   return (
-    <SafeAreaView>
+    <>
+    {
+      cluster ? (<SafeAreaView>
+        <View style={styles.container}>
+          <MapView
+            region={{
+              latitude: 36.34,
+              longitude: 127.77,
+              latitudeDelta: 4.6,
+              longitudeDelta: 4.6,
+            }}
+            style={styles.map}
+            showsUserLocation={true}
+            showsMyLocationButton={false}
+            provider={PROVIDER_GOOGLE}
+            zoomEnabled={true}>
+            {markers.map((marker, index) => (
+              <Marker
+                key={index}
+                coordinate={{
+                  latitude: parseFloat(marker.mntnLat),
+                  longitude: parseFloat(marker.mntnLon),
+                }}></Marker>
+            ))}
+          </MapView>
+        </View>
+        <View style={styles.searchContainer}>
+          <TouchableOpacity
+            style={styles.clusterButton}
+            onPress={() => setCluster(true)}>
+            <TextMedium style={styles.buttonTitle}>전체 산</TextMedium>
+          </TouchableOpacity>
+          <SearchBar setCluster={setCluster}/>
+        </View>
+      </SafeAreaView>) : 
+      
+      (<SafeAreaView>
       <View style={styles.container}>
         <MapView
           region={{
@@ -50,24 +80,27 @@ const MapHome = () => {
           showsMyLocationButton={false}
           provider={PROVIDER_GOOGLE}
           zoomEnabled={true}>
-
-          <Marker
-            coordinate={{
-              latitude: parseFloat(markerLat),
-              longitude: parseFloat(markerLon),
-            }}>
-            <Image
-              source={require('../../assets/images/marker2.png')}
-              style={styles.marker}></Image>
-          </Marker>
-
+          {markers.map((marker, index) => (
+            <Marker
+              key={index}
+              coordinate={{
+                latitude: parseFloat(marker.mntnLat),
+                longitude: parseFloat(marker.mntnLon),
+              }}></Marker>
+          ))}
         </MapView>
       </View>
-
       <View style={styles.searchContainer}>
-        <SearchBar/>
+        <TouchableOpacity
+          style={styles.clusterButton}
+          onPress={() => setCluster(true)}>
+          <TextMedium style={styles.buttonTitle}>전체 산</TextMedium>
+        </TouchableOpacity>
+        <SearchBar setCluster={setCluster}/>
       </View>
-    </SafeAreaView>
+    </SafeAreaView>)
+    }
+    </>
   );
 };
 
@@ -89,9 +122,27 @@ const styles = StyleSheet.create({
     ...StyleSheet.absoluteFillObject,
     height: windowHeight,
   },
+  searchContainer: {
+    flexDirection: 'row'
+  },
 
   marker: {
     height: 35,
     width: 35,
   },
+
+  clusterButton: {
+    margin: 10,
+    marginRight: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: windowWidth * 0.18,
+    backgroundColor: '#91C483',
+    height: windowHeight * 0.06,
+    borderRadius: 5,
+  },
+
+  buttonTitle: {
+    color: 'white'
+  }
 });
