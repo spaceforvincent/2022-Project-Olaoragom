ce14c7cb1ddd725165eee05146e391cc3efe8b9f
dearsyjang <dearsyjang@naver.com>
2022-11-18 13:58:35 +0900

    feature - Top5 그래프

--- FE/ClimbingBear/src/screens/rank/RankGraph.js
+++ FE/ClimbingBear/src/screens/rank/RankGraph.js
@@ -1,7 +1,7 @@
-import HorizontalBarGraph from '@chartiful/react-native-horizontal-bar-graph';
+import {BarChart} from 'react-native-chart-kit';
 import {useLayoutEffect, useState} from 'react';
 import {View, StyleSheet, Image, Dimensions} from 'react-native';
-import { getTotalRank } from '../../apis/Rank';
+import {getTotalRank} from '../../apis/Rank';
 import {
   TextLight,
   TextMedium,
@@ -13,46 +13,46 @@ import {
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
-const config = {
-  hasYAxisBackgroundLines: false,
-  xAxisLabelStyle: {
-    rotation: 0,
-    fontSize: 12,
-    width: 70,
-    yOffset: 4,
-    xOffset: -15,
-  },
-  yAxisLabelStyle: {
-    rotation: 30,
-    fontSize: 13,
-    prefix: '$',
-    position: 'bottom',
-    xOffset: 15,
-    decimals: 2,
-    height: 100,
-  },
+const chartConfig = {
+  backgroundGradientFrom: '#ffffff',
+  backgroundGradientTo: '#ffffff',
+  barPercentage: 0.8,
+  decimalPlaces: 0, // optional, defaults to 2dp
+  color: (opacity = 1) => `#3E7C17`,
+  labelColor: (opacity = 1) => `rgba(0, 0, 0, 1)`,
+  propsForLabels: 'TextLight',
 };
 
 const RankGraph = () => {
-  const [totalRecord, setTotalRecords] = useState([]);
-
-
+  const [topRank, setTopRank] = useState([]);
 
   useLayoutEffect(() => {
     const initialData = async () => {
       const temp = await getTotalRank();
-      const response = temp.data
-      console.log(response)
-      let recordData = [];
-      response.map((record, index) => {
-        recordData.push({
-          distance: record.distance
-        })
-      })
-      setTotalRecords(recordData)
+      const response = temp.data;
+      let templst = [];
+      response.map((rank, index) => {
+        templst.push({
+          nickname: rank.nickname,
+          record: rank.distance.toFixed(2),
+        });
+      });
+      setTopRank(templst.slice(0, 5));
     };
-    initialData()
+    initialData();
   }, []);
+
+  const labellst = topRank.map(row => row.nickname);
+  const datalst = topRank.map(row => row.record);
+
+  const data = {
+    labels: labellst,
+    datasets: [
+      {
+        data: datalst,
+      },
+    ],
+  };
 
   return (
     <View style={styles.container}>
@@ -63,15 +63,19 @@ const RankGraph = () => {
           source={require('../../assets/images/bear.png')}></Image>
       </View>
       <View style={styles.chartContainer}>
-        <HorizontalBarGraph
-          // data={totalRecord}
-          labels={['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul']}
-          width={windowWidth * 0.8}
-          height={windowHeight * 0.5}
-          barColor="green"
-          //   barRadius={15}
-          baseConfig={config}
-          style={styles.chart}
+        <BarChart
+          style={styles.graphStyle}
+          showBarTops={true}
+          showValuesOnTopOfBars={true}
+          withInnerLines={true}
+          segments={10}
+          data={data}
+          width={windowWidth * 0.9}
+          height={windowHeight * 0.8}
+          yAxisSuffix=" km"
+          chartConfig={chartConfig}
+          verticalLabelRotation={0}
+          fromZero={true}
         />
       </View>
     </View>
@@ -82,7 +86,6 @@ export default RankGraph;
 
 const styles = StyleSheet.create({
   container: {
-    margin: windowHeight * 0.03,
     marginTop: 0,
     flexDirection: 'column',
   },
@@ -95,14 +98,7 @@ const styles = StyleSheet.create({
   },
   chartContainer: {
     justifyContent: 'center',
-    marginLeft: windowWidth * 0.03,
-  },
-  chart: {
-    marginBottom: 30,
-    padding: 10,
-    paddingTop: 20,
-    borderRadius: 20,
-    backgroundColor: 'white',
+    alignItems: 'center',
   },
   title: {
     fontSize: 30,
